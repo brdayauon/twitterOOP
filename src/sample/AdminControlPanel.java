4a11e4363b739802eb023f3c12c56ad81ca32ac6
brdayauon <brdayauon@gmail,com>
2020-11-09 20:35:38 -0800

    updated

--- src/sample/AdminControlPanel.java
+++ src/sample/AdminControlPanel.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 
 public class AdminControlPanel {
 
-    private int totalUsers;
-    private int totalGroups;
+
     public ArrayList<String> users;
     public ArrayList<String> uniqueGroupIDs;
 
@@ -24,14 +23,21 @@ public class AdminControlPanel {
     }
 
     private AdminControlPanel(){
-        totalUsers = 0;
-        totalGroups = 0;
         this.users = new ArrayList<>();
         this.uniqueGroupIDs = new ArrayList<>();
 
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode(new User("Root"));
 
     }
+    /*
+    A few analysis features are needed in the admin control panel:
+    1) output the total number of users; DONE
+    2) output the total number of groups; DONE
+    3) output the total number of Tweet messages in all the users’ news feed; DONE
+    4) output the percentage of the positive Tweet messages in all the users’ news feed
+    (the message containing positive words, such as good, great, excellent, etc.)
+    Free free to decide the positive words.
+     */
+
 
 
 
@@ -41,24 +47,24 @@ public class AdminControlPanel {
 
 
 
-    public void pressAddGroupButton(ActionEvent event) {
-            System.out.println("Adding group button");
-        }
-
-    public void pressAddUser(ActionEvent event) {
-        System.out.println("Adding sampleUser");
-    }
-
-    public void pressOpenUserView(ActionEvent event){
-        System.out.println("Pressing open UserView");
-    }
-    public void pressShowMessagesTotal(ActionEvent event){
-        System.out.println("Pressing show Messages Total");
-    }
-
-    public void pressShowUserTotal(ActionEvent event) {
-        System.out.println("Pressing show User Total");
-    }
+//    public void pressAddGroupButton(ActionEvent event) {
+//            System.out.println("Adding group button");
+//        }
+//
+//    public void pressAddUser(ActionEvent event) {
+//        System.out.println("Adding sampleUser");
+//    }
+//
+//    public void pressOpenUserView(ActionEvent event){
+//        System.out.println("Pressing open UserView");
+//    }
+//    public void pressShowMessagesTotal(ActionEvent event){
+//        System.out.println("Pressing show Messages Total");
+//    }
+//
+//    public void pressShowUserTotal(ActionEvent event) {
+//        System.out.println("Pressing show User Total");
+//    }
 
 
 }
